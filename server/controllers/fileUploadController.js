1c0d7a8650b41795d2e939575927ed6d50138cba
MRX <sureshmadawa763@gmail.com>
2024-07-10 13:28:51 +0530

    basic frontend done!

--- server/controllers/fileUploadController.js
+++ server/controllers/fileUploadController.js
@@ -1,8 +1,15 @@
 const pluginUploadRespHandler = (req, res, next) => {
     try {
-        // Your handler code here
+        res.status(201).json({
+            success: true,
+            message: 'file upload successful.'
+        });
     } catch (error) {
         console.log(error);
+        res.status(500).json({
+            success: false,
+            message: error
+        });
     }
 };
 
--- server/routes/fileUploadRoute.js
+++ server/routes/fileUploadRoute.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const { pluginUploadRespHandler } = require('../controllers/fileUploadController');
+const multer = require('multer');
+const path = require('path')
 
 const fileUploadRouter = express.Router();
 
-fileUploadRouter.post('/plugin-upload', pluginUploadRespHandler);
+// Configure Multer for file uploads
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/plugin');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
+
+const upload = multer({ storage: storage });
+
+fileUploadRouter.post('/plugin-upload', upload.single('zipfile'), pluginUploadRespHandler);
+
 
 module.exports = fileUploadRouter;
